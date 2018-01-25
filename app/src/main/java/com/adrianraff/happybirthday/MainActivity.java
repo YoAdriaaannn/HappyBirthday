4cf1f8cae714be7e3d77b77410ac803b54dd03f0
fr0stsp1re <info@atomiccomputerservice.com>
2018-01-25 02:23:04 -0800

    Changed View order.  Changed Scale Type  Added QR Code  Added Additional textView with instructions.  Changed images.

--- app/src/main/java/com/adrianraff/happybirthday/MainActivity.java
+++ app/src/main/java/com/adrianraff/happybirthday/MainActivity.java
@@ -2,6 +2,8 @@ package com.adrianraff.happybirthday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-}
+
+    }
